d80b07f0b6f22d0e3d7c15f847961fbf2142d0b8
Mudrytskyi <mudritskiy.vk@gmail.com>
2024-08-09 15:44:54 +0300

    testing

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # HardHatBeginnings
 
 # Run
- yarn hardhat run scripts/deploy.ts
- yarn hardhat run scripts/deploy.ts --network localhost
 
- # Create localhost blockchain
- yarn hardhat node
+yarn hardhat run scripts/deploy.ts
+yarn hardhat run scripts/deploy.ts --network localhost
+
+# Create localhost blockchain
+
+yarn hardhat node
+
+# Testing
+
+yarn hardhat test
+yarn hardhat test --grep store // grep grabs tests which contain provided string
---
+++ test/test-deploy.ts
@@ -0,0 +1,32 @@
+import { ethers, run, network } from "hardhat";
+import { SimpleStorage } from "../typechain-types";
+import { expect, assert } from "chai";
+
+// The same as test() in Flutter Tests
+describe("Simple storage", () => {
+    let simpleStorageFactory;
+    let simpleStorageContract: SimpleStorage;
+
+    // Hits before every it()
+    beforeEach(async () => {
+        simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+        simpleStorageContract = await simpleStorageFactory.deploy();
+    });
+
+    it("Should start with a fav num of 0", async () => {
+        const currentFavNum = await simpleStorageContract.retrieve();
+        const expectedVal = 0n;
+        // assert or expect
+        assert.equal(currentFavNum, expectedVal);
+        // or
+        // expect(currentFavNum).to.equal(expectedVal);
+    });
+
+    it("Should update when we call store", async () => {
+        const transactionResponse = await simpleStorageContract.store(7);
+        await transactionResponse.wait(1);
+        const currentFavNum = await simpleStorageContract.retrieve();
+        const expectedVal = 7n;
+        assert.equal(currentFavNum, expectedVal);
+    });
+});
